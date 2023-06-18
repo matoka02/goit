564e00ec97bbe45f278b1af8564146b29d6de76c
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-18 14:19:38 +0300

    Add files via upload

--- js/goit-test-07-013.js
+++ js/goit-test-07-013.js
@@ -108,45 +108,45 @@
 
 // // 2. Игра крестики-нолики
 
-// const content = document.querySelector('.content');
-// const restart = document.querySelector('.js-restart');
+const content = document.querySelector('.content');
+const restart = document.querySelector('.js-restart');
 
-// let player = 'X';
+let player = 'X';
 
-// const win = [
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [3, 6, 9],
-//     [1, 5, 9],
-//     [3, 5, 7],
-// ];
+const win = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+];
 
-// function createMarkup() {
-//     let markup = '';
-//     for (let i = 1; i <= 9; i += 1) {
-//         markup += `<div class="item" data-id="${i}"></div>`;
-//     };    
-//     return markup;
-// };
+function createMarkup() {
+    let markup = '';
+    for (let i = 1; i <= 9; i += 1) {
+        markup += `<div class="item" data-id="${i}"></div>`;
+    };    
+    return markup;
+};
 
-// content.insertAdjacentHTML('beforeend', createMarkup());
+content.insertAdjacentHTML('beforeend', createMarkup());
 
-// content.addEventListener('click', onClick);
-// restart.addEventListener('click', onRestart);
+content.addEventListener('click', onClick);
+restart.addEventListener('click', onRestart);
 
-// function onClick(evt) {
-//     if (!evt.target.textContent) {
-//         evt.target.textContent = player;
-//         player = player === 'X' ? 'O' : 'X';
-//     } else {
-//         alert('Change!')
-//     };
-// };
+function onClick(evt) {
+    if (!evt.target.textContent) {
+        evt.target.textContent = player;
+        player = player === 'X' ? 'O' : 'X';
+    } else {
+        alert('Change!')
+    };
+};
 
-// function onRestart() {
-//     player = 'X';
-//     content.innerHTML = createMarkup();
-// };
+function onRestart() {
+    player = 'X';
+    content.innerHTML = createMarkup();
+};
