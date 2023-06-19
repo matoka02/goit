047eaffdb1445e7e4c700adc82cd99a71ed2ee0c
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-19 22:13:13 +0300

    Update goit-test-08-016.js

--- src/js/goit-test-08-016.js
+++ src/js/goit-test-08-016.js
@@ -3,45 +3,90 @@
 
 // <!-- Лекция 04.11.2022 -->
 
+const session = document.querySelector('.session');
+const local = document.querySelector('.local');
+const remove = document.querySelector('.remove');
+
+session.addEventListener('click', onSession);
+const arr = [1,2,3,4];
+
+function onSession() {
+    // sessionStorage.setItem('session', 'hello session');
+    // sessionStorage.setItem('session', arr);
+    // localStorage.getItem('local');
+    // console.log(typeof localStorage.getItem('local'));
+    // console.log(localStorage.getItem('local'));
+
+    // const data = localStorage.getItem('local');
+    // const resp = JSON.parse(data);
+    // console.log(resp);
+
+    const data = JSON.parse(localStorage.getItem('local')) || [];
+    console.log(data);
+    data.forEach(element => {
+        console.log(element);
+    });
+};
+
+local.addEventListener('click', onLocal);
+
+function onLocal() {
+    // localStorage.setItem('local', 'hello local');
+    // localStorage.setItem('local', arr);
+
+    localStorage.setItem('local', JSON.stringify(arr));    
+    localStorage.setItem('local-01', JSON.stringify(arr));    
+    localStorage.setItem('local-02', JSON.stringify(arr));    
+};
+
+remove.addEventListener('click', onRemove);  
+
+function onRemove() {
+    // localStorage.removeItem('local');       // удалание 1 ключа  
+    localStorage.clear()                    // удаление всех ключей
+}
+
+
 // // 1. Игра крестики-нолики
-const content = document.querySelector('.content');
-const restart = document.querySelector('.js-restart');
 
-content.insertAdjacentHTML('beforeend', createMarkup());
-content.addEventListener('click', onClick);
-restart.addEventListener('click', onRestart);
+// const content = document.querySelector('.content');
+// const restart = document.querySelector('.js-restart');
 
-let player = 'X';
+// content.insertAdjacentHTML('beforeend', createMarkup());
+// content.addEventListener('click', onClick);
+// restart.addEventListener('click', onRestart);
 
-const win = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-];
+// let player = 'X';
 
-function createMarkup() {
-    let markup = '';
-    for (let i = 1; i <= 9; i += 1) {
-        markup += `<div class="item" data-id="${i}"></div>`;
-    };    
-    return markup;
-};
+// const win = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9],
+//     [1, 5, 9],
+//     [3, 5, 7],
+// ];
 
-function onClick(evt) {
-    if (!evt.target.textContent) {
-        evt.target.textContent = player;
-        player = player === 'X' ? 'O' : 'X';
-    } else {
-        alert('Change!')
-    };
-};
+// function createMarkup() {
+//     let markup = '';
+//     for (let i = 1; i <= 9; i += 1) {
+//         markup += `<div class="item" data-id="${i}"></div>`;
+//     };    
+//     return markup;
+// };
 
-function onRestart() {
-    player = 'X';
-    content.innerHTML = createMarkup();
-};
+// function onClick(evt) {
+//     if (!evt.target.textContent) {
+//         evt.target.textContent = player;
+//         player = player === 'X' ? 'O' : 'X';
+//     } else {
+//         alert('Change!')
+//     };
+// };
+
+// function onRestart() {
+//     player = 'X';
+//     content.innerHTML = createMarkup();
+// };
