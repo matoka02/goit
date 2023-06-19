8684ad1c583cb9656e9e3970ebb8a01a0954de95
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-19 19:13:35 +0000

    Deploying to gh-pages from @ matoka02/goit@047eaffdb1445e7e4c700adc82cd99a71ed2ee0c 🚀

--- goit-test-08-016.3abbf992.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var t=document.querySelector(".content"),e=document.querySelector(".js-restart");t.insertAdjacentHTML("beforeend",r()),t.addEventListener("click",(function(t){t.target.textContent?alert("Change!"):(t.target.textContent=n,n="X"===n?"O":"X")})),e.addEventListener("click",(function(){n="X",t.innerHTML=r()}));var n="X";function r(){for(var t="",e=1;e<=9;e+=1)t+='<div class="item" data-id="'.concat(e,'"></div>');return t}}();
-//# sourceMappingURL=goit-test-08-016.3abbf992.js.map
