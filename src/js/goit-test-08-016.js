1dc635ee0f66e9be72430c25582fc25572f0b85f
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-20 22:27:01 +0300

    Update goit-test-08-016.js

--- src/js/goit-test-08-016.js
+++ src/js/goit-test-08-016.js
@@ -219,3 +219,41 @@ function onRestart() {
     
     content.innerHTML = createMarkup(); 
 };
+
+
+// // 4. Сервис для localStorage
+
+/**
+ * 
+ * @param {*} key 
+ * @param {*} value 
+ */
+
+// const save = (key, value) => {
+//     try {
+//         const serializedState = JSON.stringify(value);
+//         localStorage.setItem(key, serializedState);
+//     } catch (error) {
+//         console.error("Set state error: ", error.message);
+//     }
+// };
+    
+// const load = key => {
+//     try {
+//         const serializedState = localStorage.getItem(key);
+//         return serializedState === null ? undefined : JSON.parse(serializedState);
+//     } catch (error) {
+//         console.error("Get state error: ", error.message);
+//     }
+// };
+
+// const remove = key => {
+//     try {
+//         const serializedState = localStorage.removeItem(key);
+//         return serializedState === null ? undefined : JSON.parse(serializedState);
+//     } catch (error) {
+//         console.error("Get state error: ", error.message);
+//     }
+// };
+    
+//     export default { save, load, remove };
