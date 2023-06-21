e66a0f88f81dc24d2238afdb3009bfa5b15e096a
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-21 18:59:01 +0300

    Update goit-test-09-017.js

--- src/js/goit-test-09-017.js
+++ src/js/goit-test-09-017.js
@@ -2,3 +2,15 @@
 
 
 // <!-- Лекция 09.11.2022 -->
+
+console.log('start');
+
+setTimeout(()=>{
+    console.log('setTimeout');
+});
+
+for (let i = 0; i < 99; i+=1) {
+    console.log('end');    
+};
+
+console.log('asdgdhdfh');
