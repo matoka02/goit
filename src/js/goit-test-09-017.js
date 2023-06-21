7c9c76285df11c8b80aa7c647900738953e8dea0
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-21 22:52:43 +0300

    lesson
    lesson 09.11.2022

--- src/js/goit-test-09-017.js
+++ src/js/goit-test-09-017.js
@@ -67,13 +67,17 @@
 // console.log('idTime', idTime);
 
 
-// <!-- Обратный отчет -->
+// <!-- Текущие время и дата -->
 
 const refs = {
     dateDay: document.querySelector('.date-day'),
     date: document.querySelector('.date'),
     dateMonth: document.querySelector('.date-month'),
     dateYear: document.querySelector('.date-year'),
+    clock: document.querySelector('.digital-clock'),
+    seconds: document.querySelector('.clock-seconds__arrow'),
+    minutes: document.querySelector('.clock-minutes__arrow'),
+    hours: document.querySelector('.clock-hours__arrow'),
 };
 
 const nameOfDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -93,6 +97,25 @@ setInterval(()=>{
     refs.date.textContent = dayOfMonth;
     refs.dateMonth.textContent = month;
     refs.dateYear.textContent = year;
+
+    const hours = currentTime.getHours().toString().padStart(2, '0');
+    const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+    const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+    console.log(hours, minutes, seconds);
+
+    refs.clock.textContent = `Time: ${hours} : ${minutes} : ${seconds}`;
+
+    // seconds 360/60=6
+    // minutes 360/60=6
+    // hours   360/12=30
+    const forSeconds = seconds * 6;
+    const forMinutes = minutes * 6;
+    const forHours = hours * 30 + minutes * 0.5; //(30/60) = 0.5
+
+    refs.seconds.style.transform = `rotate(${forSeconds}deg)`;
+    refs.minutes.style.transform = `rotate(${forMinutes}deg)`;
+    refs.hours.style.transform = `rotate(${forHours}deg)`;
+
 }, 1000);
 
 
