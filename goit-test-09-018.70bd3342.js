3e946019c73645c58e5175e62d378c180749e6ea
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-24 17:10:28 +0000

    Deploying to gh-pages from @ matoka02/goit@d3504a88d9042db8a388bb3f20beb8aee7e4498c 🚀

--- goit-test-09-018.70bd3342.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";new Promise((function(n,o){Math.random()>.5?n(20):o("Error!")})).then((function(n){console.log(n)})).catch((function(n){return console.log(n)})).finally((function(){}))}();
-//# sourceMappingURL=goit-test-09-018.70bd3342.js.map
