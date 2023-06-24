3daa466eb1b74f3d46342a5941febc444350332a
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-24 14:57:27 +0300

    update lesson 11.11.2022

--- src/js/goit-test-09-018.js
+++ src/js/goit-test-09-018.js
@@ -1,1 +1,40 @@
 "use strict";
+
+// <!-- Лекция 11.11.2022 -->
+
+
+// // 1. Асинхронный код
+
+// // макропроцесс => call Steck => webAPI call Steck, если синхронный код и микропроцессы выполнены
+// setTimeout(()=>{'setTimeout'});
+// // микропроцесс => call Steck => webAPI => call Steck, если синхронный код выполнен
+// Promise.resolve().then(()=>{console.log('Promise');});
+// // синхронный код => call Steck 
+// console.log('object');
+
+const promise = new Promise((res, rej)=>{
+    const random = Math.random();
+
+    if (random>0.5) {
+        res(20);
+    } else{
+        rej('Error!');
+    }
+});
+// console.log(promise);           // Promise {<rejected>: 10}
+
+// promise.then((res)=>{console.log(res);},(err)=>{console.log(err);});                          // Error
+
+promise
+.then((res)=>{
+    //
+    //
+    //
+    //
+    //
+    //
+    console.log(res);                       // 20
+}).catch(err => console.log(err))          // Error
+.finally(()=>{
+    //
+})
