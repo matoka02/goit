17bbdf8c39c1e33bc5c1f50bca9bd0d58194a23e
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-02 15:56:17 +0300

    lesson 26.11.2022

--- src/js/goit-test-11-022.js
+++ src/js/goit-test-11-022.js
@@ -60,39 +60,65 @@
 // foo();
 
 
-// 4. Пример асинхронной функции, если данные нужно передать из функции наружу
+// // 4. Пример асинхронной функции, если данные нужно передать из функции наружу
+
+// const list = document.querySelector('.list');
+
+// async function foo() {
+//         const resp = await fetch('https://swapi.dev/api/people/1/'); //
+//         if (!resp.ok) {
+//             throw new Error('RESP NOT OK!')
+//         }
+//         const data = await resp.json();
+//         return data;
+// }
+// foo()
+// .then(data => {
+//     console.log(data);
+//     list.insertAdjacentHTML('beforeend', `<li>
+//     <h1>${data.name}</h1>
+//     <h2>${data.gender}</h2>
+//     </li>`);
+// })
+// .catch(err => console.log(err));
+
+
+// 5. Пример асинхронной функции с параллельной обработкой
 
 const list = document.querySelector('.list');
 
+// // 5.1 отработаются только успешные промисы
+// async function foo() {
+//     const arr = [1,2,3,4,5];
+//     const resps = await arr.map(async hero => await fetch(`https://swapi.dev/api/people/${hero}/`));
+//     console.log(resps);     // (5) [Promise, Promise, Promise, Promise, Promise]
+//     const data = await Promise.all(resps);
+//     console.log(data);      // (5) [Response, Response, Response, Response, Response]
+//     data.forEach(item => console.log(item))
+//     return data;
+// }
+// foo();
+
+// 5.2 отработаются все промисы
 async function foo() {
-        const resp = await fetch('https://swapi.dev/api/people/1/'); //
-        if (!resp.ok) {
-            throw new Error('RESP NOT OK!')
-        }
-        const data = await resp.json();
-        return data;
+    const arr = [1,2,3,4,5];
+    const resps = await arr.map(async hero => await fetch(`https://swapi.dev/api/people/${hero}/`));
+    console.log(resps);     // (5) [Promise, Promise, Promise, Promise, Promise]
+    const data = await Promise.allSettled(resps);
+    console.log(data);      // (5) {status: 'fulfilled', value: Response}
+    data.forEach(item => console.log(item))
+    return data;
 }
-foo()
-.then(data => {
-    console.log(data);
-    list.insertAdjacentHTML('beforeend', `<li>
-    <h1>${data.name}</h1>
-    <h2>${data.gender}</h2>
-    </li>`);
-})
-.catch(err => console.log(err));
+foo();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 5.3 отработает любой успешный промис, но только 1
+async function foo() {
+    const arr = [1,2,3,4,5];
+    const resps = await arr.map(async hero => await fetch(`https://swapi.dev/api/people/${hero}/`));
+    console.log(resps);     // (5) [Promise, Promise, Promise, Promise, Promise]
+    const data = await Promise.race(resps);
+    console.log(data);      // Response {type: 'cors', url: 'https://swapi.dev/api/people/1/', redirected: false, status: 200, ok: true, …}
+    // data.forEach(item => console.log(item))
+    return data;
+}
+foo();
