7fd610e32e8e6ed3206c8ab7fe346f9461cf5e67
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-12-30 22:30:14 +0200

    update

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1540,4 +1540,416 @@
 //   }
 // });
 
+
 /*--- 3.4 Клавиатура: keydown и keyup ---*/
+
+// 3.4.1 Создайте функцию runOnKeys(func, code1, code2, ... code_n), которая запускает func при одновременном нажатии клавиш с кодами code1, code2, …, code_n.
+// Например, код ниже выведет alertпри одновременном нажатии клавиш "Q" и "W" (в любом регистре, в любой раскладке)
+// runOnKeys(
+//   () => alert("Привет!"),
+//   "KeyQ",
+//   "KeyW"
+// );
+
+// function runOnKeys(func, ...codes) {
+//   let pressed = new Set();
+
+//   document.addEventListener('keydown', function(evt) {
+//     pressed.add(evt.code);
+//     // все ли клавиши из набора нажаты?
+//     for (let code of codes) { 
+//       if (!pressed.has(code)) {
+//         return;
+//       }
+//     };
+//     // да, все
+
+//     // во время показа alert, если посетитель отпустит клавиши - не возникнет keyup
+//     // при этом JavaScript "пропустит" факт отпускания клавиш, а pressed[keyCode] останется true
+//     // чтобы избежать "залипания" клавиши -- обнуляем статус всех клавиш, пусть нажиvgvgмает всё заново
+//     pressed.clear();
+
+//     func();
+//   });
+
+//   document.addEventListener('keyup', function(evt) {
+//     pressed.delete(evt.code);
+//   });
+// };
+
+// runOnKeys(
+//   () => alert("Привет!"),
+//   "KeyQ",
+//   "KeyW"
+// );
+
+
+/*--- 3.6 Прокрутка ---*/
+
+// 3.6.1 Создайте бесконечную страницу. Когда посетитель прокручивает её до конца, она автоматически добавляет текущие время и дату в текст (чтобы посетитель мог прокрутить ещё).
+// Пожалуйста, обратите внимание на две важные особенности прокрутки:
+// - Прокрутка «эластична». Можно прокрутить немного дальше начала или конца документа на некоторых браузерах/устройствах (после появляется пустое место, а затем документ автоматически «отскакивает» к нормальному состоянию).
+// - Прокрутка неточна. Если прокрутить страницу до конца, можно оказаться в 0-50px от реальной нижней границы документа.
+// Таким образом, «прокрутка до конца» должна означать, что посетитель находится на расстоянии не более 100px от конца документа.
+// P.S. В реальной жизни мы можем захотеть показать «больше сообщений» или «больше товаров».
+
+// function populate() {
+//   while(true) {
+//     let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+//     if (windowRelativeBottom > document.documentElement.clientHeight + 100) break;
+//     document.body.insertAdjacentHTML("beforeend", `<p>Date: ${new Date()}</p>`);
+//   }
+// };
+
+// window.addEventListener('scroll', populate);
+// // инициализация документа
+// populate(); 
+
+// 3.6.2 Создайте кнопку «наверх», чтобы помочь с прокруткой страницы.
+// Она должна работать следующим образом:
+// - Пока страница не прокручена вниз хотя бы на высоту окна - кнопка невидима.
+// - Когда страница прокручена вниз больше, чем на высоту окна - появляется стрелка «наверх» в левом верхнем углу. Если страница прокручивается назад, стрелка исчезает.
+// - Когда нажимается стрелка, страница прокручивается вверх.
+// Как тут (слева-сверху, прокрутите):
+
+// arrowTop.onclick = function() {
+//   window.scrollTo(pageXOffset, 0);
+//   // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
+// };
+
+// window.addEventListener('scroll', function() {
+//   arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+// });
+
+// 3.6.3 Допустим, у нас есть клиент с низкой скоростью соединения, и мы хотим сэкономить его трафик.
+// Для этого мы решили не показывать изображения сразу, а заменять их на «макеты», как тут:
+// <img src="placeholder.svg" width="128" height="128" data-src="real.jpg">
+// То есть, изначально, все изображения - placeholder.svg. Когда страница прокручивается до того положения, где пользователь может увидеть изображение - мы меняем src на значение из data-src, и таким образом изображение загружается.
+// Вот пример в iframe:
+// - Прокрутите его, чтобы увидеть загрузку изображений «по требованию».
+// Требования:
+// - При загрузке страницы те изображения, которые уже видимы, должны загружаться сразу же, не ожидая прокрутки.
+// - Некоторые изображения могут быть обычными, без data-src. Код не должен касаться их.
+// - Если изображение один раз загрузилось, оно не должно больше перезагружаться при прокрутке.
+// P.S. Если можете, реализуйте более продвинутое решение, которое будет загружать изображения на одну страницу ниже/после текущей позиции.
+// P.P.S. Достаточно обрабатывать вертикальную прокрутку, горизонтальную не требуется.
+
+// // Проверка видимости элемента (в видимой части страницы)
+// // Достаточно, чтобы верхний или нижний край элемента был виден
+// function isVisible(elem) {
+//   let coords = elem.getBoundingClientRect();
+//   let windowHeight = document.documentElement.clientHeight;
+//   // видны верхний ИЛИ нижний край элемента
+//   let topVisible = coords.top > 0 && coords.top < windowHeight;
+//   let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+//   return topVisible || bottomVisible;
+// };
+
+// // Вариант проверки, считающий элемент видимым,
+// // если он не более чем -1 страница назад или +1 страница вперед.
+// function isVisible(elem) {
+//   let coords = elem.getBoundingClientRect();
+//   let windowHeight = document.documentElement.clientHeight;
+//   let extendedTop = -windowHeight;
+//   let extendedBottom = 2 * windowHeight;
+//   // top visible || bottom visible
+//   let topVisible = coords.top > extendedTop && coords.top < extendedBottom;
+//   let bottomVisible = coords.bottom < extendedBottom && coords.bottom > extendedTop;
+//   return topVisible || bottomVisible;
+// };
+
+// function showVisible() {
+//   for (let img of document.querySelectorAll('img')) {
+//     let realSrc = img.dataset.src;
+//     if (!realSrc) continue;
+//     if (isVisible(img)) {
+//       // отключение кеширования
+//       // эта строка должна быть удалена в "боевом" коде
+//       realSrc += '?nocache=' + Math.random();
+//       img.src = realSrc;
+//       img.dataset.src = '';
+//     }
+//   }
+// };
+
+// window.addEventListener('scroll', showVisible);
+// showVisible();
+
+
+/*--- 4.1 Свойства и методы формы ---*/
+
+// 4.1.1 Имеется <select>:
+// <select id="genres">
+//   <option value="rock">Рок</option>
+//   <option value="blues" selected>Блюз</option>
+// </select>
+// Используя JavaScript:
+// - Выведите значение и текст выбранного пункта.
+// - Добавьте пункт: <option value="classic">Классика</option>.
+// - Сделайте его выбранным.
+
+// let selectedOption = genres.options[genres.selectedIndex];
+// console.log(selectedOption.value);
+// console.log(selectedOption.text);
+// let newOption = new Option('Классика', 'classic');
+// genres.append(newOption);
+// newOption.selected = true;
+
+
+/*--- 4.2 Фокусировка: focus/blur ---*/
+
+// 4.2.1 Создайте <div>, который превращается в <textarea>, если на него кликнуть.
+// <textarea> позволяет редактировать HTML в элементе <div>.
+// Когда пользователь нажимает Enter или переводит фокус, <textarea> превращается обратно в <div>, и его содержимое становится HTML-кодом в <div>.
+
+// let area = null;
+// let view = document.getElementById('view');
+
+// view.onclick = function () {
+//   editStart();
+// };
+
+// function editStart() {
+//   area = document.createElement('textarea');
+//   area.className = 'edit';
+//   area.value = view.innerHTML;
+
+//   area.onkeydown = function (evt) {
+//     if (evt.key == 'Enter') {
+//       this.blur();
+//     }
+//   };
+
+//   area.onblur = function () {
+//     editEnd();
+//   };
+
+//   view.replaceWith(area);
+//   area.focus();
+// };
+
+// function editEnd() {
+//   view.innerHTML = area.value;
+//   area.replaceWith(view);
+// };
+
+
+// 4.2.2 Сделайте ячейки таблицы редактируемыми по клику.
+// - По клику - ячейка должна стать «редактируемой» (textarea появляется внутри), мы можем изменять HTML. Изменение размера ячейки должно быть отключено.
+// - Кнопки OK и ОТМЕНА появляются ниже ячейки и, соответственно, завершают/отменяют редактирование.
+// - Только одну ячейку можно редактировать за один раз. Пока <td> в «режиме редактирования», клики по другим ячейкам игнорируются.
+// - Таблица может иметь множество ячеек. Используйте делегирование событий.
+
+// let table = document.getElementById('bagua-table');
+
+// let editingTd;
+
+// table.onclick = function(evt) {
+//   // 3 возможных цели
+//   let target = evt.target.closest('.edit-cancel,.edit-ok,td');
+//   if (!table.contains(target)) return;
+//   if (target.className == 'edit-cancel') {
+//     finishTdEdit(editingTd.elem, false);
+//   } else if (target.className == 'edit-ok') {
+//     finishTdEdit(editingTd.elem, true);
+//   } else if (target.nodeName == 'TD') {
+//     if (editingTd) return; 
+//   // уже редактируется
+//     makeTdEditable(target);
+//   };
+// };
+
+// function makeTdEditable(td) {
+//   editingTd = {
+//     elem: td,
+//     data: td.innerHTML
+//   };
+//   // td в состоянии редактирования, CSS применятся к textarea внутри ячейки
+//   td.classList.add('edit-td'); 
+
+//   let textArea = document.createElement('textarea');
+//   textArea.style.width = td.clientWidth + 'px';
+//   textArea.style.height = td.clientHeight + 'px';
+//   textArea.className = 'edit-area';
+
+//   textArea.value = td.innerHTML;
+//   td.innerHTML = '';
+//   td.appendChild(textArea);
+//   textArea.focus();
+
+//   td.insertAdjacentHTML("beforeEnd",
+//     '<div class="edit-controls"><button class="edit-ok">OK</button><button class="edit-cancel">CANCEL</button></div>'
+//   );
+// };
+
+// function finishTdEdit(td, isOk) {
+//   if (isOk) {
+//     td.innerHTML = td.firstChild.value;
+//   } else {
+//     td.innerHTML = editingTd.data;
+//   };
+//   td.classList.remove('edit-td');
+//   editingTd = null;
+// };
+
+// 4.2.3 Установите фокус на мышь. Затем используйте клавиши со стрелками, чтобы её двигать:
+// P.S. Не добавляйте обработчики никуда, кроме элемента #mouse.
+// P.P.S. Не изменяйте HTML/CSS, подход должен быть общим и работать с любым элементом.
+
+// mouse.tabIndex = 0;
+
+// mouse.onclick = function () {
+//   this.style.left = this.getBoundingClientRect().left + 'px';
+//   this.style.top = this.getBoundingClientRect().top + 'px';
+
+//   this.style.position = 'fixed';
+// };
+
+// mouse.onkeydown = function (evt) {
+//   switch (evt.key) {
+//     case 'ArrowLeft':
+//       this.style.left = parseInt(this.style.left) - this.offsetWidth + 'px';
+//       return false;
+//     case 'ArrowUp':
+//       this.style.top = parseInt(this.style.top) - this.offsetHeight + 'px';
+//       return false;
+//     case 'ArrowRight':
+//       this.style.left = parseInt(this.style.left) + this.offsetWidth + 'px';
+//       return false;
+//     case 'ArrowDown':
+//       this.style.top = parseInt(this.style.top) + this.offsetHeight + 'px';
+//       return false;
+//   }
+// };
+
+
+/*--- 4.3 События: change, input, cut, copy, paste ---*/
+
+// 4.3.1 Создайте интерфейс, позволяющий ввести сумму банковского вклада и процент, а затем рассчитать, какая это будет сумма через заданный промежуток времени.
+// Любое изменение введённых данных должно быть обработано немедленно.
+// Формула:
+// initial: начальная сумма денег
+// interest: проценты, например, 0.05 означает 5% в год
+// years: сколько лет ждать
+// let result = Math.round(initial * (1 + interest) ** years);
+
+// let form = document.forms.calculator;
+
+// form.money.oninput = calculate;
+// form.months.onchange = calculate;
+// form.interest.oninput = calculate;
+
+// function calculate() {
+//   let initial = +form.money.value;
+//   if (!initial) return;
+
+//   let interest = form.interest.value * 0.01;
+
+//   if (!interest) return;
+
+//   let years = form.months.value / 12;
+//   if (!years) return;
+
+//   let result = Math.round(initial * (1 + interest) ** years);
+
+//   let height = result / form.money.value * 100 + 'px';
+//   document.getElementById('height-after').style.height = height;
+//   document.getElementById('money-before').innerHTML = form.money.value;
+//   document.getElementById('money-after').innerHTML = result;
+// };
+
+// calculate();
+
+
+/*--- 4.4 Отправка формы: событие и метод submit ---*/
+
+// 4.4.1 Создайте функцию showPrompt(html, callback), которая выводит форму с сообщением (html), полем ввода и кнопками OK/ОТМЕНА.
+// - Пользователь должен ввести что-то в текстовое поле и нажать Enter или кнопку «OK», после чего должна вызываться функция callback(value) со значением поля.
+// - Если пользователь нажимает Esc или кнопку «ОТМЕНА», тогда вызывается callback(null).
+// В обоих случаях нужно завершить процесс ввода и закрыть диалоговое окно с формой.
+// Требования:
+// - Форма должна быть в центре окна.
+// - Форма является модальным окном, это значит, что никакое взаимодействие с остальной частью страницы невозможно, пока пользователь не закроет его.
+// - При показе формы, фокус должен находиться сразу внутри <input>.
+// - Клавиши Tab/Shift+Tab должны переключать фокус между полями формы, не позволяя ему переходить к другим элементам страницы.
+// Пример использования:
+// showPrompt("Введите что-нибудь<br>...умное :)", function(value) {
+//   alert(value);
+// });
+// P.S. HTML/CSS исходного кода к этой задаче содержит форму с фиксированным позиционированием, но вы должны сделать её модальной.
+
+// // Показать полупрозрачный DIV, чтобы затенить страницу
+// // (форма располагается не внутри него, а рядом, потому что она не должна быть полупрозрачной)
+// function showCover() {
+//   let coverDiv = document.createElement('div');
+//   coverDiv.id = 'cover-div';
+//   // убираем возможность прокрутки страницы во время показа модального окна с формой
+//   document.body.style.overflowY = 'hidden';
+//   document.body.append(coverDiv);
+// };
+
+// function hideCover() {
+//   document.getElementById('cover-div').remove();
+//   document.body.style.overflowY = '';
+// };
+
+// function showPrompt(text, callback) {
+//   showCover();
+//   let form = document.getElementById('prompt-form');
+//   let container = document.getElementById('prompt-form-container');
+//   document.getElementById('prompt-message').innerHTML = text;
+//   form.text.value = '';
+
+//   function complete(value) {
+//     hideCover();
+//     container.style.display = 'none';
+//     document.onkeydown = null;
+//     callback(value);
+//   };
+
+//   form.onsubmit = function () {
+//     let value = form.text.value;
+//     // игнорируем отправку пустой формы
+//     if (value == '') return false; 
+//     complete(value);
+//     return false;
+//   };
+
+//   form.cancel.onclick = function () {
+//     complete(null);
+//   };
+
+//   document.onkeydown = function (evt) {
+//     if (evt.key == 'Escape') {
+//       complete(null);
+//     }
+//   };
+
+//   let lastElem = form.elements[form.elements.length - 1];
+//   let firstElem = form.elements[0];
+
+//   lastElem.onkeydown = function (evt) {
+//     if (evt.key == 'Tab' && !e.shiftKey) {
+//       firstElem.focus();
+//       return false;
+//     }
+//   };
+
+//   firstElem.onkeydown = function (evt) {
+//     if (evt.key == 'Tab' && evt.shiftKey) {
+//       lastElem.focus();
+//       return false;
+//     }
+//   };
+
+//   container.style.display = 'block';
+//   form.elements.text.focus();
+// }
+
+// document.getElementById('show-button').onclick = function () {
+//   showPrompt("Введите что-нибудь<br>...умное :)", function (value) {
+//     alert("Вы ввели: " + value);
+//   });
+// };
+
+
